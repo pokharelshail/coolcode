b265fedf1e04daba5baf4369e6b7010838d1e428
pokharelshail <shreeshail.pokharel@bison.howard.edu>
2021-01-28 20:14:50 -0500

    Added comments and messages

--- Large-scale-programming/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ Large-scale-programming/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,12 +1,16 @@
 //Name: Shreeshail Pokharel
 package org.howard.edu.lsp.assignment1;
 
+
+/**
+ * Hello World program that contains my implementation
+ * @author shailpokharel
+ */
+
 public class HelloWorld {
-
 	public static void main(String[] args) {
-	
+		//print line
 		System.out.println("Hello World!!");
-
 	}
 
 }
