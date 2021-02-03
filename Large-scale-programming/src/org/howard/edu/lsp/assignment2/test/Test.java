d1b55d4b00857fd108df08c86bcd31b4da10698f
pokharelshail <shreeshail.pokharel@bison.howard.edu>
2021-02-03 01:11:36 -0500

    Update test cases and java docs

--- Large-scale-programming/src/org/howard/edu/lsp/assignment2/test/Test.java
+++ Large-scale-programming/src/org/howard/edu/lsp/assignment2/test/Test.java
@@ -8,15 +8,27 @@ import java.util.Arrays;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		Combination test = new Combination();
+		
+		Combination test = new Combination(); // Making Combination object to start testing
+		
+		
+		/* 11 different test cases with description of each case */
+		
+		//Test0
+		int[] arr0 = new int[] {};
+		int target0=15;
+		ArrayList<String> test0= test.calculateCombinations(arr0,target0);
+		System.out.println("Test 0 - Empty Array");
+		System.out.println("Inputs  : "+ Arrays.toString(arr0)+ " Target: " +target0);
+		System.out.println("Outputs : "+ test0);
+		System.out.println();
 		
 		//Test1
 		int[] arr1 = new int[] {5,5,10,15};
 		int target1 =15;
 		ArrayList<String> test1= test.calculateCombinations(arr1,target1);
-		System.out.println("Test 1");
+		System.out.println("Test 1 - Regular");
 		System.out.println("Inputs  : "+ Arrays.toString(arr1)+ " Target: " +target1);
 		System.out.println("Outputs : "+ test1);
 		System.out.println();
@@ -25,7 +37,7 @@ public class Test {
 	    int[] arr2 = new int[] {1,2,3,4,5,6};
 		int target2= 6;
 		ArrayList<String> test2= test.calculateCombinations(arr2,target2);
-		System.out.println("Test 2");
+		System.out.println("Test 2 - If target is last num");
 		System.out.println("Inputs  : "+ Arrays.toString(arr2)+ " Target: " +target2);
 		System.out.println("Outputs : "+ test2);
 		System.out.println();
@@ -35,7 +47,7 @@ public class Test {
 	    int[] arr = new int[] {4};
 		int target= 4;
 		ArrayList<String> totalCombinations= test.calculateCombinations(arr,target);
-		System.out.println("Test 3");
+		System.out.println("Test 3 - One element and one target");
 		System.out.println("Inputs  : "+ Arrays.toString(arr)+ " Target: " +target);
 		System.out.println("Outputs : "+ totalCombinations);
 		System.out.println();
@@ -44,7 +56,7 @@ public class Test {
 		int [] arr4 = new int[]{1,1,1,1,1};
 		int target4= 6;
 	    ArrayList<String> totalCombinations2= test.calculateCombinations(arr4,target4);
-		System.out.println("Test 4");
+		System.out.println("Test 4 - Target not reached");
 		System.out.println("Inputs  : "+ Arrays.toString(arr4)+ " Target: " +target4);
 		System.out.println("Outputs : "+ totalCombinations2);
 		System.out.println();
@@ -53,7 +65,7 @@ public class Test {
 		int [] arr5 = new int[]{1,2,3};
 		int target5= 3;
 	    ArrayList<String> totalCombinations5= test.calculateCombinations(arr5,target5);
-		System.out.println("Test 5");
+		System.out.println("Test 5 - Regular");
 		System.out.println("Inputs  : "+ Arrays.toString(arr5)+ " Target: " +target5);
 		System.out.println("Outputs : "+ totalCombinations5);
 		System.out.println();	
@@ -62,7 +74,7 @@ public class Test {
 		int [] arr6 = new int[]{0,1,0,1,0,1};
 		int target6= 1;
 	    ArrayList<String> totalCombinations6= test.calculateCombinations(arr6,target6);
-		System.out.println("Test 6");
+		System.out.println("Test 6 - Dataset with multiple zeros and target");
 		System.out.println("Inputs  : "+ Arrays.toString(arr6)+ " Target: " +target6);
 		System.out.println("Outputs : "+ totalCombinations6);
 		System.out.println();	
@@ -71,7 +83,7 @@ public class Test {
 		int [] arr7 = new int[]{9,9,9,9,9,9,9};
 		int target7= 9;
 	    ArrayList<String> totalCombinations7= test.calculateCombinations(arr7,target7);
-		System.out.println("Test 7");
+		System.out.println("Test 7 - All nums are target");
 		System.out.println("Inputs  : "+ Arrays.toString(arr7)+ " Target: " +target7);
 		System.out.println("Outputs : "+ totalCombinations7);
 		System.out.println();	
@@ -80,7 +92,7 @@ public class Test {
 		int [] arr8 = new int[]{2,2,2,2,2};
 		int target8= 4;
 	    ArrayList<String> totalCombinations8= test.calculateCombinations(arr8,target8);
-		System.out.println("Test 8");
+		System.out.println("Test 8 - adding any two equals target");
 		System.out.println("Inputs  : "+ Arrays.toString(arr8)+ " Target: " +target8);
 		System.out.println("Outputs : "+ totalCombinations8);
 		System.out.println();	
@@ -89,16 +101,16 @@ public class Test {
 		int [] arr9 = new int[]{3,10,10,10,10,10,10,10,10,10};
 		int target9= 3;
 	    ArrayList<String> totalCombinations9= test.calculateCombinations(arr9,target9);
-		System.out.println("Test 9");
+		System.out.println("Test 9 - First num is target");
 		System.out.println("Inputs  : "+ Arrays.toString(arr9)+ " Target: " +target9);
 		System.out.println("Outputs : "+ totalCombinations9);
 		System.out.println();	
 		
 		//Test10
-		int [] arr10 = new int[]{4,4,4,4,4,4};
+		int [] arr10 = new int[]{-1,3,-1};
 		int target10= 2;
 	    ArrayList<String> totalCombinations10= test.calculateCombinations(arr10,target10);
-		System.out.println("Test 10");
+		System.out.println("Test 10- Negative numbers ");
 		System.out.println("Inputs  : "+ Arrays.toString(arr10)+ " Target: " +target10);
 		System.out.println("Outputs : "+ totalCombinations10);
 		System.out.println();	
